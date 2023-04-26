e166bcfd4c6126502c92b66fe68495b2fba0649d
Samuel Paez Perez <spaez@viva.com.do>
2023-04-26 11:33:24 -0400

    remove other platform

---
+++ lib/config/constants/environament.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class Enviroment {
+  static String? movieDbKey =
+      dotenv.env['THE_MOVIEDB_KEY'] ?? 'They key is not configured';
+}
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:moviesapp/config/constants/environament.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
@@ -7,8 +9,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Placeholder(),
+    return Scaffold(
+      body: Center(child: Text(Enviroment.movieDbKey ?? '')),
     );
   }
 }
