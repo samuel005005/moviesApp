f460a4f81b4bb06c657b84ebe3d2a7cc2ea25f20
Samuel Paez <spaez@viva.com.do>
2023-05-02 23:51:07 -0400

    s

--- lib/presentation/widgets/movies/movies_slidershow.dart
+++ lib/presentation/widgets/movies/movies_slidershow.dart
@@ -1,6 +1,7 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:moviesapp/domain/entities/movie.dart';
 
 class MoviesSlideShow extends StatelessWidget {
@@ -49,22 +50,25 @@ class _Slide extends StatelessWidget {
       ],
     );
 
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 30),
-      child: DecoratedBox(
-        decoration: decoration,
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          child: Image.network(movie.backdropPath, fit: BoxFit.cover,
-              loadingBuilder: (context, child, loadingProgress) {
-            if (loadingProgress != null) {
-              return const DecoratedBox(
-                decoration: BoxDecoration(color: Colors.black12),
-              );
-            }
+    return GestureDetector(
+      onTap: () => context.push('/movie/${movie.id}'),
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 30),
+        child: DecoratedBox(
+          decoration: decoration,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.network(movie.backdropPath, fit: BoxFit.cover,
+                loadingBuilder: (context, child, loadingProgress) {
+              if (loadingProgress != null) {
+                return const DecoratedBox(
+                  decoration: BoxDecoration(color: Colors.black12),
+                );
+              }
 
-            return FadeIn(child: child);
-          }),
+              return FadeIn(child: child);
+            }),
+          ),
         ),
       ),
     );
