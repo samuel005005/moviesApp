8909ec3d44f3f69b58e2c47fa3745b5763a55b9a
Samuel Paez Perez <spaez@viva.com.do>
2023-05-05 13:55:43 -0400

    Go route

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -5,7 +5,35 @@ import 'package:moviesapp/presentation/views/views.dart';
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-    ShellRoute(),
+    ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (context, state) {
+            return const HomeView();
+          },
+          routes: [
+            GoRoute(
+              path: 'movie/:id',
+              name: MovieScreen.name,
+              builder: (context, state) {
+                final movieId = state.params['id'] ?? 'no-id';
+                return MovieScreen(movieId: movieId);
+              },
+            ),
+          ],
+        ),
+        GoRoute(
+          path: '/favorite',
+          builder: (context, state) {
+            return const FavoriteView();
+          },
+        )
+      ],
+    ),
     // GoRoute(
     //   path: '/',
     //   name: HomeScreen.name,
--- lib/presentation/widgets/shared/custom_bottom_navigator.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigator.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomButtonNavigator extends StatelessWidget {
   const CustomButtonNavigator({super.key});
+
+  int getCurrentIndex(BuildContext context) {
+    final String location = GoRouterState.of(context).location;
+    switch (location) {
+      case '/':
+        return 0;
+      case '/categories':
+        return 1;
+      case '/favorite':
+        return 2;
+      default:
+        return 0;
+    }
+  }
+
+  void onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        context.go('/');
+        break;
+      case 1:
+        context.go('/');
+        break;
+      case 2:
+        context.go('/favorite');
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       elevation: 0,
+      currentIndex: getCurrentIndex(context),
+      onTap: (value) => onItemTapped(context, value),
       items: const [
         BottomNavigationBarItem(
           label: "Home",
