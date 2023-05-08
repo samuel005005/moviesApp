834d8f2ffefad2405a09eb316557f6d673144f50
Samuel Paez Perez <spaez@viva.com.do>
2023-05-08 15:27:55 -0400

    crud with Isar

--- lib/infrastructure/datasources/isar_storage/local_storage_datasource.dart
+++ lib/infrastructure/datasources/isar_storage/local_storage_datasource.dart
@@ -1,22 +1,48 @@
+import 'package:isar/isar.dart';
 import 'package:moviesapp/domain/datasources/local_stotage_datasource.dart';
 import 'package:moviesapp/domain/entities/movie.dart';
+import 'package:path_provider/path_provider.dart';
 
 class StorageDataSource extends LocalStorageDataSource {
-  @override
-  Future<bool> isMovieFavorite(int movieId) {
-    // TODO: implement isMovieFavorite
-    throw UnimplementedError();
+  late Future<Isar> database;
+  StorageDataSource() {
+    database = openDatabase();
+  }
+
+  Future<Isar> openDatabase() async {
+    final dir = await getApplicationDocumentsDirectory();
+    if (Isar.instanceNames.isEmpty) {
+      return await Isar.open([MovieSchema],
+          inspector: true, directory: dir.path);
+    }
+    return Future.value(Isar.getInstance());
   }
 
   @override
-  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) {
-    // TODO: implement loadMovies
-    throw UnimplementedError();
+  Future<bool> isMovieFavorite(int movieId) async {
+    final isar = await database;
+    final Movie? isFavoriteMovie =
+        await isar.movies.filter().idEqualTo(movieId).findFirst();
+    return isFavoriteMovie != null;
   }
 
   @override
-  Future<void> toggleFavorite(Movie movie) {
-    // TODO: implement toggleFavorite
-    throw UnimplementedError();
+  Future<void> toggleFavorite(Movie movie) async {
+    final isar = await database;
+    final favoriteMovie =
+        await isar.movies.filter().idEqualTo(movie.id).findFirst();
+    if (favoriteMovie != null) {
+      isar.writeTxnSync(() => isar.movies.deleteSync(favoriteMovie.isarId!));
+      return;
+    }
+
+    isar.writeTxnSync(() => isar.movies.putSync(movie));
+    return;
+  }
+
+  @override
+  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) async {
+    final isar = await database;
+    return await isar.movies.where().offset(offset).limit(limit).findAll();
   }
 }
